2e1294bbd46ea564d8cee4c2ca8d3c78aa0e40da
Ramandip singh alien <5.0singhr@gmail.com>
2023-01-31 10:28:03 -0500

    this is second update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  loginform = new FormGroup({
+    name:new FormControl(''),
+    email: new FormControl('')
+  });
   title = 'PRACTICE';
+  loginUser(){console.log(this.loginform.value)
+}
 }
